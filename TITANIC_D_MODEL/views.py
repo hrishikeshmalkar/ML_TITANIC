1d74b36efe216599cfeacab2c019704e09cc56fa
HRISHIKESH <hrishikeshmalkar123@gmail.com>
2020-07-01 18:00:45 +0530

    ML_Titanic_Survival_Prediction

---
+++ TITANIC_D_MODEL/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from . import ML_Model
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def model(request):
+    return render(request, 'model.html')
+
+
+def result(request):
+    pclass = int(request.GET['pclass'])
+    sex = int(request.GET['sex'])
+    age = int(request.GET['age'])
+    sibsp = int(request.GET['sibsp'])
+    parch = int(request.GET['parch'])
+    fare = int(request.GET['fare'])
+    embarked = int(request.GET['embarked'])
+    title = int(request.GET['title'])
+    prediction = ML_Model.predict_Model(pclass, sex, age, sibsp, parch, fare, embarked, title)
+    return render(request, 'result.html', {'result': prediction})
